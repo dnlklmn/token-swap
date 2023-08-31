29a4a033a0402266aa7106c7a62cc4fe6e9c949f
dnlklmn <pelicansinberlin@gmail.com>
2023-08-31 15:18:14 +0200

    title fix

--- src/InContextMenu.tsx
+++ src/InContextMenu.tsx
@@ -177,7 +177,7 @@ function ContextMenuLocal({ children }: ContextMenuLocalProps) {
 export default function InContextMenu() {
   return (
     <div className="w-full flex gap-6 px-8 h-full text-foreground-contrast">
-      <div className="w-2/3 bg-background-dip rounded-md h-128" />
+      <div className="w-2/3 h-[512px] bg-background-dip rounded-md h-128" />
       <div className="flex w-1/3 flex-col gap-6">
         <div className="flex flex-col h-fit p-4 gap-2 rounded-lg text-lg font-medium overflow-hidden bg-background-float shadow-[inset_0_0_1px_1px_rgba(0,0,0,0.07)] dark:shadow-[inset_0_0_1px_1px_rgba(255,255,255,0.05)]">
           <span className="text-lg px-2 font-medium my-4">
