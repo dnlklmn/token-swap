8d449054e1976eab1d33a9c77bb2b115b3412fd8
dnlklmn <pelicansinberlin@gmail.com>
2023-09-01 09:04:33 +0200

    Update tooltip-content.tsx

--- src/components/ui/tooltip-content.tsx
+++ src/components/ui/tooltip-content.tsx
@@ -31,49 +31,54 @@ export function TokenTooltipContent({
         </Button>
       </div>
       <div className="h-[1px] bg-fill-separator" />
-      <div className="flex gap-5 items-center text-sm">
-        <div className="flex items-center">
-          <div className="w-3 overflow-visible ">
-            <PolkadotCircle size="24" />
+      <div className="flex flex-col gap-2">
+        <div className="flex gap-5 py-1 items-center text-sm ">
+          <div className="flex items-center gap-1">
+            <div className="w-3 overflow-visible ">
+              <PolkadotCircle
+                size="20"
+                className=" drop-shadow-[-1px_0px_0px_rgba(255,255,255,1)]"
+              />
+            </div>
+            <div className="w-3 overflow-visible ">
+              <MoonbeamCircle
+                size="20"
+                className=" drop-shadow-[-1px_0px_0px_rgba(255,255,255,1)]"
+              />
+            </div>
+            <div className="w-3 overflow-visible ">
+              <AcalaCircle
+                size="20"
+                className="drop-shadow-[-1px_0px_0px_rgba(255,255,255,1)]"
+              />
+            </div>
           </div>
-          <div className="w-3 overflow-visible ">
-            <MoonbeamCircle size="24" />
+          <span>
+            Available on <strong>Polkadot</strong> and{" "}
+            <strong>12 other networks</strong>
+          </span>
+        </div>
+        <div className="flex gap-5 items-center text-foreground-disabled">
+          <div className="flex gap-1.5 items-center hover:text-foreground-primary cursor-pointer">
+            <Governance className="w-5" stroke={"currentColor"} />
+            <span>Governance</span>
           </div>
-          <div className="w-3 overflow-visible ">
-            <AcalaCircle size="24" />
+          <div className="flex gap-1.5 items-center hover:text-foreground-primary cursor-pointer">
+            <Staking className="w-5" stroke={"currentColor"} />
+            <span>Staking</span>
           </div>
-        </div>
-        <span>
-          Available on <strong>Polkadot</strong> and{" "}
-          <strong>12 other networks</strong>
-        </span>
-      </div>
-      <div className="flex justify-between items-center">
-        <div className="flex gap-1.5 items-center">
-          <Governance
-            className="w-6"
-            stroke={"var(--colors-foreground-dimmed)"}
-          />
-          <span>Governance</span>
-        </div>
-        <div className="flex gap-1.5 items-center">
-          <Staking className="w-6" stroke={"var(--colors-foreground-dimmed)"} />
-          <span>Staking</span>
-        </div>
-        <div className="flex gap-1.5 items-center">
-          <Crowdloans
-            className="w-6"
-            stroke={"var(--colors-foreground-dimmed)"}
-          />
-          <span>Crowdloan</span>
+          <div className="flex gap-1.5 items-center hover:text-foreground-primary cursor-pointer">
+            <Crowdloans className="w-5" stroke={"currentColor"} />
+            <span>Crowdloan</span>
+          </div>
         </div>
       </div>
       {withHint && (
         <>
           <div className="h-[1px] bg-fill-separator" />
-          <div className="flex items-center gap-1">
-            <div className="w-6 flex justify-center">
-              <RightClickIcon className="text-foreground-dimmed" />
+          <div className="flex items-center gap-1.5 text-sm text-foreground-disabled">
+            <div className="w-5 flex justify-center">
+              <RightClickIcon size="20" />
             </div>
             <span>Right click to see available actions</span>
           </div>
